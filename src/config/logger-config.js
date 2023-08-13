16e2b7ef46b509b9665a5b142e2e5215bd389aa3
OmkarTerbhai <omkarterbhai@gmail.com>
2023-08-13 12:03:46 +0530

    Added logging utility.

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,4 +1,4 @@
-const { createLogger, format, transports } = require('winston');
+import { createLogger, format, transports } from 'winston';
 const { combine, timestamp, label, printf } = format;
 
 const customFormat = printf(( { level, message, timestamp, error } ) => {
@@ -16,4 +16,4 @@ const logger = createLogger({
     ],
 });
 
-module.exports = logger;
+export default logger;
