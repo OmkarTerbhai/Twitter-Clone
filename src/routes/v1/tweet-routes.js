96995b7ad244886ccacdf1b6e265d21e189b7ba7
OmkarTerbhai <omkarterbhai@gmail.com>
2023-08-17 21:57:07 +0530

    Added authentication for create tweet.

--- src/routes/v1/tweet-routes.js
+++ src/routes/v1/tweet-routes.js
@@ -2,10 +2,11 @@ import express from "express";
 import { createTweet } from "../../controllers/tweet-controller.js";
 import { getTweets } from "../../controllers/tweet-controller.js";
 import { getTweet } from "../../controllers/tweet-controller.js";
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const tweetRouter = express.Router();
 
-tweetRouter.post('/create', createTweet);
+tweetRouter.post('/create', authenticate, createTweet);
 tweetRouter.get('/list', getTweets);
 tweetRouter.get('/:tweetId', getTweet);
 
